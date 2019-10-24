6d63654323eef4ac977721c20eec7de3515982c7
Milena Königshofen <39162269+milenazilena@users.noreply.github.com>
2019-10-24 10:08:58 +0200

    Update bash_download_partial_file.sh

--- swarm_stats/bash_download_partial_file.sh
+++ swarm_stats/bash_download_partial_file.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-wget -q -O - https://elwe.rhrk.uni-kl.de/outgoing/16S_V3_V4_1332_samples_1f.stats.gz | gunzip -f - | head -n 20000 - > 16S_V3_V4_1332_samples_1f_20000.stats 
 
+URL="https://elwe.rhrk.uni-kl.de/outgoing/16S_V3_V4_1332_samples_1f.stats.gz"
+LINES=20000
+OUTPUT="${URL/*\//}"
+OUTPUT="${OUTPUT/.gz/}"
+
+wget --quiet --output-document - "${URL}" | \
+gunzip --force --stdout | \
+head --lines "${LINES}" > "${OUTPUT}"
+
+exit 0
