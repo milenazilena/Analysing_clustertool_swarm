f821a1a264fcec66dac99d51653f0e5213cc777d
Milena Königshofen <39162269+milenazilena@users.noreply.github.com>
2019-10-28 14:48:30 +0100

    Add files via upload

--- test_fastidious_option.bash
+++ test_fastidious_option.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 FASTA_FILE=$(printf ">s1_1000\nAA\n>s2_100\nCC\n>s3_10\nGG\n>s4_1\nTT\n")
-while read -r line; do
-	b= $(echo "${FASTA_FILE}" | grep "^>"| grep -Eo "[0-9]+$")
-	# for b in "${b}"; do
-		# /usr/bin/time -f "%e\t%M\t%P" \
-                       # swarm \
-                       # -f -b "${b}" \
-                       # "${FASTA_FILE}" \
-                       # -o /dev/null \
-                       # -l /dev/null 2>&1
-	# done
-done
-
+echo "${FASTA_FILE}" | grep "^>"| grep -Eo "[0-9]+$" | \
+while read abundance; do 
+	echo $(( abundance - 1 )) 
+done \
+|grep -v "^0$" | \
+while read b; do 
+	/usr/bin/time -f "%e\t%M\t%P" \
+                swarm \
+                -f -b "${b}" \
+                "${FASTA_FILE}" \
+                -o /dev/null \
+                -l /dev/null 2>&1
+done \
+| tac
 exit 0
