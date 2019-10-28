53a64bc1a9b0a2abf7a4d584b59afab6c724d404
Milena Königshofen <39162269+milenazilena@users.noreply.github.com>
2019-10-28 11:32:26 +0100

    Add files via upload

---
+++ test_fastidious_option.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+FASTA_FILE=$(printf ">s1_1000\nAA\n>s2_100\nCC\n>s3_10\nGG\n>s4_1\nTT\n")
+echo "${FASTA_FILE}"| while read -r line; do
+	b= $(grep '^>' | -Eo "(_).*")
+	for "${b}" in {2..1000};do
+		/usr/bin/time -f "%e\t%M\t%P" \
+                       swarm \
+                       -f -b ${b} \
+                       "${FASTA_FILE}" \
+                       -o /dev/null \
+                       -l /dev/null 2>&1
+	done
+done
+
+exit 0
+
+grep 
