1f0ca63a44c4f7adbe25ceed1b6eaba7adec69e0
Milena Königshofen <39162269+milenazilena@users.noreply.github.com>
2019-10-29 11:10:16 +0100

    Add files via upload

---
+++ Tests/test_fastidious_option_with_file.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# v0.1.0
+# 29.10.2019
+# This script uses a fasta-file-like-variable to measure computation time and memory of swarm 
+# used with different boundary values and creates a .stats file for each boundary value.
+args=("$@")
+echo "${args[0]} "
+FASTA_FILE="${args[1]}"
+
+SUBFOLDER="Test_stats"
+mkdir "${SUBFOLDER}"
+STATS="${SUBFOLDER}/statfile_test_b"
+
+awk -F "_" '/^>/ {a=$2 - 1 ; if (a > 0) {print a}}' <<< $FASTA_FILE | \
+tac | \
+while read b; do 
+	for i in {1..3}; do # repeat it 3 times
+		echo -ne "file\t${i}\t${b}\t" # shows boundary values as column next to other values
+		/usr/bin/time -f "%e\t%M\t%P" \
+		swarm -f -b "${b}" \
+			-s ${STATS}_${b}.stats \
+			-o /dev/null \
+			-l /dev/null 2>&1 <<< "${FASTA_FILE}"
+	done 
+done > temp.log
+
+exit 0
