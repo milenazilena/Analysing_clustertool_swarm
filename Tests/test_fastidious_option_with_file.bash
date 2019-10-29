d9e66b05f7210b72ee8cac7befb538b7a9c5cbf7
Milena Königshofen <39162269+milenazilena@users.noreply.github.com>
2019-10-29 11:52:43 +0100

    Add files via upload

--- Tests/test_fastidious_option_with_file.bash
+++ Tests/test_fastidious_option_with_file.bash
@@ -4,18 +4,20 @@
 # 29.10.2019
 # This script uses a fasta-file-like-variable to measure computation time and memory of swarm 
 # used with different boundary values and creates a .stats file for each boundary value.
-args=("$@")
-FASTA_FILE="${args[0]}"
 
+args=("$@")
+
+FASTA_FILE="${args[0]}"
 SUBFOLDER="Test_stats"
 mkdir "${SUBFOLDER}"
 STATS="${SUBFOLDER}/statfile_test_b"
+FILE_NAME="$(awk -F'[.]' '{print $2}' ${FASTA_FILE})
 
 awk -F "_" '/^>/ {a=$2 - 1 ; if (a > 0) {print a}}' ${FASTA_FILE} | \
 tac | \
 while read b; do 
 	for i in {1..3}; do # repeat it 3 times
-		echo -ne "file\t${i}\t${b}\t" # shows boundary values as column next to other values
+		echo -ne "${FILE_NAME}\t${i}\t${b}\t" # shows boundary values as column next to other values
 		/usr/bin/time -f "%e\t%M\t%P" \
 		swarm -f -b "${b}" \
 			"${FASTA_FILE}" \
@@ -26,3 +28,8 @@ while read b; do
 done > temp.log
 
 exit 0
+
+#filename in log without .fasta
+# why illegal header line?
+#empty .stats, empty .log
+# why not 999?
