e0bd6a7df52a3cd9a888267cbf35509c15354bd0
Milena Königshofen <39162269+milenazilena@users.noreply.github.com>
2019-10-29 14:52:51 +0100

    Add files via upload

--- Tests/test_fastidious_option_with_file.bash
+++ Tests/test_fastidious_option_with_file.bash
@@ -5,21 +5,22 @@
 # This script uses a fasta-file-like-variable to measure computation time and memory of swarm 
 # used with different boundary values and creates a .stats file for each boundary value.
 
-# filename in log without .fasta
+
 # why illegal header line?
 #empty .stats, empty .log
 # why not 999?
 
 FASTA_FILE="${1}"
 SUBFOLDER="Test_stats"
-mkdir "${SUBFOLDER}"
-STATS="${SUBFOLDER}/statfile_test_b"
 FILE_NAME="${FASTA_FILE/.*/}"
+mkdir -p "${SUBFOLDER}"
+STATS="${SUBFOLDER}/${FILE_NAME}_b"
+
 
 awk -F "_" '/^>/ {a=$2 - 1 ; if (a > 0) {print a}}' ${FASTA_FILE} | \
 tac | \
-while read b; do 
-	for i in {1..3}; do # repeat it 3 times
+while read b ; do 
+	for i in {1..3} ; do # repeat it 3 times
 		echo -ne "${FILE_NAME}\t${i}\t${b}\t" # shows boundary values as column next to other values
 		/usr/bin/time -f "%e\t%M\t%P" \
 		swarm -f -b "${b}" \
