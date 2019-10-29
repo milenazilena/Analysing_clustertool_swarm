3e4b7e5e60ccc8131da348ae5d07fed1253f87c7
Milena Königshofen <39162269+milenazilena@users.noreply.github.com>
2019-10-29 09:11:24 +0100

    Add files via upload

--- Tests/test_fastidious_option.bash
+++ Tests/test_fastidious_option.bash
@@ -1,17 +1,22 @@
 #!/bin/bash
+
+
+#
 FASTA_FILE=$(printf ">s1_1000\nAA\n>s2_100\nCC\n>s3_10\nGG\n>s4_1\nTT\n")
-echo "${FASTA_FILE}"| while read -r line; do
-	b= $(grep '^>' | -Eo "(_).*")
-	for b in {2..1000};do
+
+awk -F "_" '/^>/ {a=$2 - 1 ; if (a > 0) {print a}}' <<< $FASTA_FILE | \
+tac | \
+while read b; do 
+	for i in {1..3}; do #repeat it 3 times
+		echo -ne "${i}\t${b}\t" #shows boundary values as column next to other values
 		/usr/bin/time -f "%e\t%M\t%P" \
-                       swarm \
-                       -f -b ${b} \
-                       "${FASTA_FILE}" \
-                       -o /dev/null \
-                       -l /dev/null 2>&1
-	done
-done
+		swarm -f -b "${b}" \
+		-s statfile_test_${b}.stats \
+		-o /dev/null \
+		-l /dev/null 2>&1<<< "${FASTA_FILE}"
+	done 
+done > temp.log
 
 exit 0
 
-grep 
+#stat file for each b value
