0fc235a62243acea373f54e538bcd55e5d28cef4
Milena Königshofen <39162269+milenazilena@users.noreply.github.com>
2019-11-13 09:29:19 +0100

    Update bash_download_partial_file.sh

--- swarm_stats/bash_download_partial_file.sh
+++ swarm_stats/bash_download_partial_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL="https://elwe.rhrk.uni-kl.de/outgoing/Camargue_16S_roots_and_stems_20190920_1593_samples_1f.stats.gz"
+URL="https://elwe.rhrk.uni-kl.de/outgoing/all_forest_soils_18S_V4_904_samples_1f.stats.gz"
 LINES=50000
 OUTPUT="${URL/*\//}"
 OUTPUT="${OUTPUT/.gz/}"
